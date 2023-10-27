9c443b801501ba29926a4055fddd7bc4392799ed
Gaurav Pandey <gaurav.2022@tecorb.co>
2023-10-27 16:45:15 +0530

    demo project on the zustand

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import CourseForm from './components/courseForm';
+import CourseList from './components/courseList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Course List</h1>
+      <CourseForm />
+      <CourseList />
     </div>
   );
 }
